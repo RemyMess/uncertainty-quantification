f1cfb0c913bf8b77a0f55cf11f9fa6a3587b5ddf
rmess <remy.messadene@gmail.com>
2020-04-04 13:14:26 +0100

    Finalisation of the metastability tests + adding the graphs

--- src/metastability.py
+++ src/metastability.py
@@ -6,7 +6,13 @@ import time
 
 
 class DoubleWellDynamics(object):
-    def __init__(self, h=1, w=1, sig=1, beta=1):
+    """
+    Computational experience for the dynamics of a particle in a 1D/2D double well potential whose dynamics
+    is prescribed by
+    dX_t = - nabla V(X_t)dt + \sqrt(2 \ beta ^-1) dW_t
+    """
+    def __init__(self, h=1, w=100, sig=100, beta=100):
+
         # meta vars
         assert(h >= 0 and w >= 0)
         self.h = h
@@ -20,10 +26,11 @@ class DoubleWellDynamics(object):
 
     def potential(self, dim, x):
         if dim == 1:
-            return x ** 2 - 1  
+            return self.h * (1 - (abs(x) - self.r0 - self.w) ** 2 / self.w**2) ** 2  
+        
         elif dim == 2:
             assert(isinstance(x, list))
-            r = sum([i**2 for i in x]) 
+            r = sum([i**2 for i in x]) ** (1/2)
 
             # page 44 book
             return self.h * (1 - (r - self.r0 - self.w) ** 2 / self.w**2) ** 2
@@ -32,21 +39,28 @@ class DoubleWellDynamics(object):
 
     def grad_potential(self, dim, x):
         """
-        Expression of the grad from p.44
+        Expression of the double well potential can be found in "T. Lelièvre, M. Rousset and G. Stoltz, Free energy computations: A mathematical perspective,
+        Imperial College Press, 2010." at page 44.
         """
         if dim == 1:
-            return 2 * x 
+            return 2 * self.h * (1 - (abs(x) - self.r0 - self.w) ** 2 / (self.w **2) ) * (-2) * (abs(x) - self.r0 - self.w) / (self.w ** 2) * 2
         elif dim == 2:
-            r = math.sqrt(sum([i**2 for i in x]))
-            grads = [2 * self.h * (1 - (r - self.r0 - self.w) ** 2 / self.w **2) * 2 * (r - self.r0 - self.w) / (self.w ** 2) * 2 * x[i] / math.sqrt(r) for i in range(dim)]
+            r = sum([i**2 for i in x]) ** (1/2)
+
+            # dealing with derivative around r=0
+            if True:
+                grads = [2 * self.h * (1 - (r - self.r0 - self.w) ** 2 / self.w **2) * (-2) * (r - self.r0 - self.w) / (self.w ** 2) * 2 * x[i] / r for i in range(dim)]
+            else:
+                grads = [2 * self.h * (1 - (r - self.r0 - self.w) ** 2 / self.w **2) * (-2) * (r - self.r0 - self.w) / (self.w ** 2) * 2 * math.sqrt(x[i]) for i in range(dim)]
+
             return np.array(grads)
         else:
             raise NotImplementedError("grad_potential: variable 'dim' is implemented for dimension 1 or 2 only.")
 
     def run(self, x0, end_time, time_increment=1, start_time=0):
         """
-        Dynamics: dX_t = - \nabla V(X_t)dt + \sqrt(2 \beta ^-1) dW_t
-        Discretisation: X_t = X_t-1 + -\nabla V(X_t-1) * \Delta t + \sqrt(2 \beta ^-1) \Delta W_{t-1}
+        Dynamics: dX_t = - nabla V(X_t)dt + \sqrt(2 \ beta ^-1) dW_t
+        Discretisation: X_t = X_t-1 + -nabla V(X_t-1) * Delta t + \sqrt(2 beta ^-1) Delta W_{t-1}
         """
         # check format x0 and get dim
         if type(x0) is int or x0 is float:
@@ -61,24 +75,18 @@ class DoubleWellDynamics(object):
 
         logging.warning(f"run: running dynamics in dimension {dim}.")
 
-        # # init bm
-        # bm = BrownianMotion(start_point=[0,0], 
-        #                     dim=dim, 
-        #                     time_increment=time_increment,
-        #                     init_time=start_time) 
-
         # run
         t = start_time
         self.x = [(start_time, x0)]
         x_previous = x0
-        cste = math.sqrt(2 * self.beta)
+        cste = math.sqrt(2 * self.beta ** -1)
 
         while t < end_time:
             x = x_previous - self.grad_potential(dim, x_previous) * time_increment + cste * np.random.normal(0, math.sqrt(time_increment), size=dim)
             t += time_increment
 
-            #print(f"x: {x}")
-            #print(f"grad: {self.grad_potential(dim, x)}")
+            print(f"x: {x}")
+            print(f"grad: {self.grad_potential(dim, x)}")
 
             if dim == 2:
                 self.x.append((t, list(x)))
@@ -88,14 +96,92 @@ class DoubleWellDynamics(object):
 
         return np.array(self.x)
 
+    def plot_1d(self, interval=50, plot_pot=True, plot_grad=True, plot_dynamics=True):
+        #plot potential 1D
+        if plot_pot == True:
+            pot = [self.potential(dim=1, x=i) for i in range(-500, 500, 1)]
+            plt.plot(pot)
+            plt.title("Double well potential in 1D")
+            plt.show()
+
+        #plot gradients 1D
+        if plot_grad == True:
+            grad_pot = [self.grad_potential(dim=1, x=i) for i in range(-500, 500, 1)]
+            plt.plot(grad_pot)
+            plt.title("Gradient of the potential in 1D")
+            plt.show()
+
+        # plot dynamics 1D
+
+        if plot_dynamics == True:
+            start_pos1 = 0
+            dyn = self.run(start_pos1, 1000000, time_increment=60)
+            dyn = [i[1] for i in dyn]
+
+            plt.plot(dyn)
+            plt.title(f"Metastability in 1D for particle starting at x={start_pos1}")
+            plt.show()
+
+            start_pos2 = 250
+            dyn = self.run(start_pos2, 1000000, time_increment=60)
+            dyn = [i[1] for i in dyn]
+            plt.plot(dyn)
+            plt.title(f"Metastability in 1D for particle starting at x={start_pos2}")
+            plt.show()
+
+    def plot_2d(self, interval=10, plot_pot=True, plot_grad=True, plot_dynamics=True):
+        # plot grad 2D
+        if plot_pot == True:
+            x = np.linspace(-interval, interval, 30)
+            y = np.linspace(-interval, interval, 30)
+            X, Y = np.meshgrid(x,y)
+
+            def func_wrapper(x1, x2):
+                return self.potential(dim=2, x=[x1,x2])
+        
+            Z = func_wrapper(X, Y)
+            fig = plt.figure()
+            ax = plt.axes(projection='3d')
+            ax.contour3D(X, Y, Z, 50, cmap='binary')
+            ax.set_xlabel('x')
+            ax.set_ylabel('y')
+            ax.set_zlabel('z')
+            plt.title("Double well potential in 2D")
+            plt.show()
+
+        # plot gradients 2D
+        if plot_grad == True:
+            # Make the grid
+            x, y = np.meshgrid(np.arange(-interval, interval, interval/10),
+                                np.arange(-interval, interval, interval/10))
+
+            def func_wrapper(x1, x2):
+                return self.grad_potential(dim=2, x=[x1,x2])
+
+            U,V = func_wrapper(x, y)
+            fig, ax = plt.subplots()
+            ax.quiver(x, y, U, V)
+            ax.set_xlabel('x')
+            ax.set_ylabel('y')
+            ax.set_ylabel('z')
+            plt.title("Gradient of the potential in 2D")
+
+            plt.show()
+
+        if plot_dynamics == True:
+            iterations = 1000000
+            logging.warning(f"plot2d: simulation of {iterations} time steps about to start. This might take up to minute.")
+            time.sleep(2)
+            dyn = obj.run([50, 50], iterations, time_increment=40)
+            dyn = [i[1] for i in dyn]
+            x, y = zip(*dyn)
+            plt.plot(x,y)
+            plt.title(f"Metastability for particle 2D")
+            plt.show()
+
+
 if __name__ == "__main__":
-    # Testing unit
-    obj = DoubleWellDynamics()
-    dyn = obj.run(1, 1000, time_increment=0.1)
-
-    dyn_x = [x[1] for x in dyn]
-
     import matplotlib.pyplot as plt
-
-    plt.plot(dyn_x)
-    plt.show()
+    obj = DoubleWellDynamics()
+    obj.plot_1d(300)
+    obj.plot_2d(300)
